dd49f4ccdfd8f5e989fa345c63661be21e01f446
SRI-SUDHA-RAVURI <159107326+SRI-SUDHA-RAVURI@users.noreply.github.com>
2024-08-07 16:36:07 +0530

    Add files via upload

---
+++ welcome.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Runtime.InteropServices;
+
+
+namespace operators
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C# code");
+        }
+    }
+}
