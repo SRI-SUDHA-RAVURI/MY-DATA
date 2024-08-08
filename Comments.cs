2c5f0bbee06feaf6b9e034e948cc6606da469f69
SRI-SUDHA-RAVURI <159107326+SRI-SUDHA-RAVURI@users.noreply.github.com>
2024-08-08 10:53:14 +0530

    Add files via upload

---
+++ Comments.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Operators
+{
+    internal class Comments 
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("sparrow");
+            Console.Write("hello students");
+            Console.Write("python");*/
+            Console.Write("welcome to my world");
+        }
+    }
+}
